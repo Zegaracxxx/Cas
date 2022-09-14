e7788dba1aa096618aa3f45784784174f2b74500
Zegaracxxx <113441676+Zegaracxxx@users.noreply.github.com>
2022-09-14 19:07:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-zyvdce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zyvdce)
---
+++ index.js
@@ -0,0 +1,9 @@
+let god= Number(prompt("Unesite godine"));
+if(Number.isNaN (god)){
+  console.log("Niste uneli broj!");
+} else
+if (god)=18) {
+  console.log("Punoletni ste")
+} else {
+  console.log("NIste punoletni")
+};
